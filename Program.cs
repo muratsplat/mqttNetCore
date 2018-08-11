f29c6ea03fae00299e24f47aeac0c9b71ff032d1
Murat Ödünç <murat.asya@gmail.com>
2018-08-11 23:11:44 +0300

    try somethings to know the functionality of the library..

--- Program.cs
+++ Program.cs
@@ -2,7 +2,8 @@
 using MQTTnet.Server;
 using MQTTnet;
 using MQTTnet.Protocol;
-
+using System.Text;
+using System.Threading.Tasks;
 
 
 namespace mqttNetCore
@@ -23,8 +24,7 @@ namespace mqttNetCore
             // Configure MQTT server.
             var optionsBuilder = new MqttServerOptionsBuilder()
                 .WithConnectionBacklog(100)
-                .WithDefaultEndpointPort(1884)
-                .WithDefaultEndpointPort(1883);
+                .WithDefaultEndpointPort(1884);
 
             var options = new MqttServerOptions();
             options.ConnectionValidator = c => 
@@ -35,13 +35,14 @@ namespace mqttNetCore
                 //     return;
                 // }
 
+                // TODO: Adds WebHook to auth
                 if (c.Username != "user")
                 {
                     c.ReturnCode = MqttConnectReturnCode.ConnectionRefusedBadUsernameOrPassword;
                     return;
                 }
 
-                if (c.Password != "pass")
+                if (c.Password != "secret")
                 {
                     c.ReturnCode = MqttConnectReturnCode.ConnectionRefusedBadUsernameOrPassword;
                     return;
@@ -51,7 +52,19 @@ namespace mqttNetCore
 
 
             var mqttServer = new MqttFactory().CreateMqttServer();
-        
+                mqttServer.ApplicationMessageReceived += (s, e) => {
+                    // TODO: Adds Webhook to sending message ..
+                    Console.WriteLine("### RECEIVED APPLICATION MESSAGE ###");
+                    Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
+                    Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+                    Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
+                    Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
+                    Console.WriteLine();
+            };
+
+            options.SubscriptionInterceptor = ctx => {
+                Console.WriteLine("ClientID={0}", ctx.ClientId);
+            };  
             await  mqttServer.StartAsync(options);
             
             Console.WriteLine("Press any key to exit.");
